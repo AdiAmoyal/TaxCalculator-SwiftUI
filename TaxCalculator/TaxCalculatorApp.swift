34ddaa091355b44576af090048c525246bab409e
Adi Amoyal <adi.amoyal02@gmail.com>
2024-04-01 13:10:14 -0700

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by Adi Amoyal on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
